36048bceef7b30a484bd49893e68d3f1c2859b2a
xiao278 <90475159+xiao278@users.noreply.github.com>
2025-08-11 00:29:25 +0800

    refactored form

--- frontend/src/components/Content/HousingForm/HousingForm.tsx
+++ frontend/src/components/Content/HousingForm/HousingForm.tsx
@@ -1,68 +1,81 @@
 import { SmallCard } from "../../Template/SmallCard/SmallCard"
 import { FormInput } from "../../Template/FormInput/FormInput"
 import "./HousingForm.css"
-import { useState } from "react";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+
+interface HousingFormInput {
+    bathrooms: number;
+    bedrooms: number;
+    size: number;
+    unit: string;
+    purchase_date: string;
+    purchase_price: number;
+    purchase_currency: string;
+    address_id: number;
+
+    building_name: string;
+    street_number: string;
+    street_name: string;
+    postal_code: string;
+    city: string;
+    state: string;
+    country: string;
+}
 
 export function HousingForm() {
-    const [unit, setUnit] = useState('');
-    const [bathrooms, setBathrooms] = useState(0);
-    const [bedrooms, setBedrooms] = useState(0);
-    const [size, setSize] = useState(0);
-    const [currency, setCurrency] = useState('');
-    const [price, setPrice] = useState(0);
-    const [purchaseDate, setPurchaseDate] = useState<string>(new Date().toISOString().substring(0,10))
-
-    const [buildingName, setBuildingName] = useState('');
-    const [streetNumber, setStreetNumber] = useState('');
-    const [streetName, setStreetName] = useState('');
-    const [city, setCity] = useState('');
-    const [zip, setZip] = useState('');
-    const [state, setState] = useState('');
-    const [country, setCountry] = useState('');
+    const methods = useForm<HousingFormInput>();
+    const { handleSubmit } = methods;
+    const onFormSubmit: SubmitHandler<HousingFormInput> = (data) => console.log(data)
 
     return (
-        <div className="HousingFormContainer">
-            <div className="HfSection">
-                <h2>Housing Details</h2>
-                <SmallCard>
-                    <div className="ClusteredInput">
-                        <FormInput name="unit no." type="text" allowEmpty={true} inputField={unit} setInputField={setUnit} />
-                        <FormInput name="bathrooms" type="number" allowEmpty={false} inputField={bathrooms} setInputField={setBathrooms} />
-                        <FormInput name="bedrooms" type="number" allowEmpty={false} inputField={bedrooms} setInputField={setBedrooms} />
-                        <FormInput name="size (m^2)" type="number" allowEmpty={false} inputField={size} setInputField={setSize} />
+        <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onFormSubmit)}>
+                <div className="HousingFormContainer">
+                    <div className="HfSection">
+                        <h2>Housing Details</h2>
+                        <SmallCard>
+                            <div className="ClusteredInput">
+                                <FormInput fieldName="unit" hint="unit no." type="text" />
+                                <FormInput fieldName="bathrooms" hint="bathrooms" type="number" validation={{required: true}} />
+                                <FormInput fieldName="bedrooms" hint="bedrooms" type="number" validation={{required: true}} />
+                                <FormInput fieldName="size" hint="size (m^2)" type="number" validation={{required: true}} />
+                            </div>
+                            <div className="ClusteredInput">
+                                <div style={{width: "100%", maxWidth:"60px", minWidth:"0"}}>
+                                    <FormInput fieldName="purchase_currency" hint="currency" type="text" validation={{required: true}} />
+                                </div>
+                                <div style={{width: "100%"}}>
+                                    <FormInput fieldName="purchase_price" hint="price" type="number" validation={{required: true}} />
+                                </div>
+                                <FormInput fieldName="purchase_date" hint="date of purchase" type="date" validation={{required: true}} />
+                            </div>
+                        </SmallCard>
                     </div>
-                    <div className="ClusteredInput">
-                        <div style={{width: "100%", maxWidth:"60px", minWidth:"0"}}>
-                            <FormInput name="currency" type="text" allowEmpty={false} inputField={currency} setInputField={setCurrency} />
+                    <div className="HfSection">
+                        <div>
+                            <h2>Address</h2>
                         </div>
-                        <div style={{width: "100%"}}>
-                            <FormInput name="price" type="number" allowEmpty={false} inputField={price} setInputField={setPrice} />
-                        </div>
-                        <FormInput name="date of purchase" type="date" allowEmpty={false} inputField={purchaseDate} setInputField={setPurchaseDate} />
+                        <SmallCard>
+                            <FormInput fieldName="building_name" hint="building name" type="text" />
+                            <div className="ClusteredInput">
+                                <div style={{width: "100%", maxWidth:"60px", minWidth:"0"}}>
+                                    <FormInput fieldName="street_number" hint="street no." type="text" />
+                                </div>
+                                <FormInput fieldName="street_name" hint="street name" type="text" validation={{required: true}} />
+                            </div>
+                            <FormInput fieldName="city" hint="city" type="text" validation={{required: true}} />
+                            <div className="ClusteredInput">
+                                <div style={{width: "100%", maxWidth:"60px", minWidth:"0"}}>
+                                    <FormInput fieldName="postal_code" hint="zip code" type="text" validation={{required: true}} />
+                                </div>
+                                <FormInput fieldName="state" hint="state" type="text" validation={{required: true}} />
+                                <FormInput fieldName="country" hint="country" type="text" validation={{required: true}} />
+                            </div>
+                        </SmallCard>
                     </div>
-                </SmallCard>
-            </div>
-            <div className="HfSection">
-                <h2>Address</h2>
-                <SmallCard>
-                    <FormInput name="building name" type="text" allowEmpty={true} inputField={buildingName} setInputField={setBuildingName}/>
-                    <div className="ClusteredInput">
-                        <div style={{width: "100%", maxWidth:"60px", minWidth:"0"}}>
-                            <FormInput name="street no." type="text" allowEmpty={true} inputField={streetNumber} setInputField={setStreetNumber}/>
-                        </div>
-                        <FormInput name="street name" type="text" allowEmpty={false} inputField={streetName} setInputField={setStreetName}/>
-                    </div>
-                    <FormInput name="city" type="text" allowEmpty={false} inputField={city} setInputField={setCity}/>
-                    <div className="ClusteredInput">
-                        <div style={{width: "100%", maxWidth:"60px", minWidth:"0"}}>
-                            <FormInput name="zip code" type="text" allowEmpty={false} inputField={zip} setInputField={setZip}/>
-                        </div>
-                        <FormInput name="state" type="text" allowEmpty={false} inputField={state} setInputField={setState}/>
-                        <FormInput name="country" type="text" allowEmpty={false} inputField={country} setInputField={setCountry}/>
-                    </div>
-                </SmallCard>
-            </div>
-            <button className="HfSection HousingFormSubmitButton">Submit</button>
-        </div>
+                    <button type="submit" className="HfSection HousingFormSubmitButton">Submit</button>
+                </div>
+            </form>
+        </FormProvider>
     )
 }
--- frontend/src/components/Template/FormInput/FormInput.tsx
+++ frontend/src/components/Template/FormInput/FormInput.tsx
@@ -1,25 +1,22 @@
-import { Dispatch } from "react";
+import { FieldValues, RegisterOptions, useFormContext } from "react-hook-form"
 import "./FormInput.css"
 
+
 interface FormInputProps {
-    name: string;
+    fieldName: string;
+    hint: string;
     type: "text" | "number" | "date"
-    allowEmpty: boolean;
-    inputField: any;
-    setInputField: (value: any) => void
+    validation?: RegisterOptions<FieldValues, string>;
 }
 
 export function FormInput(props: FormInputProps) {
-    const { name, type, allowEmpty, inputField, setInputField } = props;
+    const { hint, type, fieldName, validation } = props;
+    const { register } = useFormContext();
     return (
         <div className="FormInputContainer">
-            <input className={"FormInput"} value={inputField} type={type} style={{
-                backgroundColor: (!allowEmpty && (type == "text") && (inputField.length < 1)) ? "rgb(245,220,220)" : ""
-            }} onChange={(e) => setInputField(e.target.value)} />
-            <div className="FormInputName"> {name}
-                {allowEmpty ? <></> : 
-                    <span style={{color: "red"}}>*</span>
-                }
+            <input className={"FormInput"} {...register(fieldName, validation)} type={type} />
+            <div className="FormInputName"> {hint}
+                {(validation && validation.required) ? <span style={{color: "red"}}>*</span> : <></>}
             </div>
         </div>
     )
